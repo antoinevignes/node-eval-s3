2384b76a88c3631f60ec375802a9c3f0946f4eee
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2025-10-27 17:13:50 +0100

    page details materiau OK

--- frontend/src/pages/materials/MaterialDetail.jsx
+++ frontend/src/pages/materials/MaterialDetail.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 export default function MaterialDetail() {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const { id } = useParams();
+  const [material, setMaterial] = useState({});
+
+  useEffect(() => {
+    async function getMaterial() {
+      const response = await fetch(`${API_URL}/material/${id}`);
+
+      const data = await response.json();
+
+      setMaterial(data);
+    }
+
+    getMaterial();
+  }, []);
+
+  console.log(material);
+
   return (
     <>
-      <div>MATERIAL</div>
+      {material && (
+        <section className="flex flex-col justify-center items-center h-[80vh] space-y-4">
+          <h1 className="text-2xl font-bold">
+            {material.name} ({material.type})
+          </h1>
+
+          <p>Fabriqué par : {material.company_id?.name}</p>
+
+          <p className="w-[50%]">"{material.description}"</p>
+        </section>
+      )}
     </>
   );
 }
