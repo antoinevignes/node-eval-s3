8fdaf9993cd3d973e0e15622bb2d5de8dbfde849
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2025-10-28 11:39:40 +0100

    ajout route par type

--- backend/controllers/stats.controller.js
+++ backend/controllers/stats.controller.js
@@ -1,6 +1,6 @@
 import FurnitureMaterial from "../models/furniture_material.model.js";
 
-export async function getMaterialUsage(req, res) {
+export async function getMaterialUsage(_, res) {
   try {
     const results = await FurnitureMaterial.aggregate([
       {
@@ -26,6 +26,41 @@ export async function getMaterialUsage(req, res) {
           _id: 0,
           material_id: "$_id.id",
           name: "$_id.name",
+          totalUsed: 1,
+        },
+      },
+      { $sort: { totalUsed: -1 } },
+    ]);
+
+    res.status(200).json(results);
+  } catch (error) {
+    console.error("Erreur récupération stat:", error);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+}
+
+export async function getMaterialUsageByType(_, res) {
+  try {
+    const results = await FurnitureMaterial.aggregate([
+      {
+        $lookup: {
+          from: "materials",
+          localField: "material_id",
+          foreignField: "_id",
+          as: "material",
+        },
+      },
+      { $unwind: "$material" },
+      {
+        $group: {
+          _id: "$material.type",
+          totalUsed: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          type: "$_id",
           totalUsed: 1,
         },
       },
--- backend/routes/stats.route.js
+++ backend/routes/stats.route.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   getMaterialUsage,
   getMaterialUsageByCompany,
+  getMaterialUsageByType,
 } from "../controllers/stats.controller.js";
 
 const router = Router();
 
 router.get("/by-material", getMaterialUsage);
+router.get("/by-type", getMaterialUsageByType);
 router.get("/by-company", getMaterialUsageByCompany);
 
 export default router;
